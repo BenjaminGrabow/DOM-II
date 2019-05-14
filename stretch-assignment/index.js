ff8d0487aeee450c30ce30a018042ee5cf7a2220
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-05-14 16:19:01 +0200

    Added 5 eventlisteners for doubleclick event

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -49,7 +49,7 @@ blockGray.addEventListener("click", function() {
 
 // block goes to the right if mouse is clicked down
 blockRed.addEventListener("mousedown", function() {
-    TweenMax.to(".block--red", 3, {x:600, ease: Bounce.easeOut});
+    TweenMax.to(".block--red", 3, {x:600, ease: Bounce.easeOut});     
 });
 blockBlue.addEventListener("mousedown", function() {
     TweenMax.to(".block--blue", 3, {x:600, ease: Bounce.easeOut});
@@ -62,4 +62,31 @@ blockPink.addEventListener("mousedown", function() {
 });
 blockGray.addEventListener("mousedown", function() {
     TweenMax.to(".block--gray", 3, {x:600, ease: Bounce.easeOut});
+});
+
+// block goes from right to left if double clicked
+blockRed.addEventListener("dblclick", function() {
+    if (this.style.transform = "translate3d(600px, 0px, 0px)") {
+        TweenMax.to(".block--red", 3, {x:0, ease: Bounce.easeOut});
+    };
+});
+blockBlue.addEventListener("dblclick", function() {
+    if (this.style.transform = "translate3d(600px, 0px, 0px)") {
+        TweenMax.to(".block--blue", 3, {x:0, ease: Bounce.easeOut});
+    };
+});
+blockGreen.addEventListener("dblclick", function() {
+    if (this.style.transform = "translate3d(600px, 0px, 0px)") {
+        TweenMax.to(".block--green", 3, {x:0, ease: Bounce.easeOut});
+    };
+});
+blockPink.addEventListener("dblclick", function() {
+    if (this.style.transform = "translate3d(600px, 0px, 0px)") {
+        TweenMax.to(".block--pink", 3, {x:0, ease: Bounce.easeOut});
+    };
+});
+blockGray.addEventListener("dblclick", function() {
+    if (this.style.transform = "translate3d(600px, 0px, 0px)") {
+        TweenMax.to(".block--gray", 3, {x:0, ease: Bounce.easeOut});
+    };
 });
