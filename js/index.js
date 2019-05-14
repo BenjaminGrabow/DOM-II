1da38aa8bf891568589576dab3ceea6a64692ab6
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-05-14 13:17:50 +0200

    Added evenlisteners to the nav bar with stopPropagation and preventDefault method

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+// NAV BAR with stopPropagation and preventDefault
+let nav = document.getElementsByClassName("nav-link");
+
+document.querySelector("nav").addEventListener("dblclick", bgToRed)
+nav[0].addEventListener("click", function(event) {
+    this.style.color = "green";
+    event.preventDefault();
+});
+nav[1].addEventListener("dblclick", function(event) {
+    event.stopPropagation();
+    this.style.color = "blue";
+});
+nav[1].addEventListener("click", function(event) {
+    event.preventDefault();
+});
+nav[2].addEventListener("mouseout", function(event) {
+    this.style.color = "white";
+    event.preventDefault();
+});
+nav[2].addEventListener("click", function(event) {
+    event.preventDefault();
+});
+nav[3].addEventListener("mouseenter", colorToYellow);
+nav[3].addEventListener("click", function(event) {
+    event.preventDefault();
+});
+
+function colorToYellow() {
+    this.style.color = "yellow";
+}
+function bgToRed() {
+    this.style.backgroundColor = "red";
+}
